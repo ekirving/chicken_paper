fed5d54f23523551cf15f5832b2a1dea24b82cfb
Evan Irving-Pease <ekirving@hotmail.com>
2017-12-20 15:23:57 +1100

    added the map

--- veroni-plot.R
+++ veroni-plot.R
@@ -2,18 +2,20 @@
 # install.packages(c("sp", "maps", "maptools", "mapproj"), dependencies=TRUE)
 
 library(deldir)
-library(rworldmap)
+# library(rworldmap)
+require(maps)
+
 
 setwd("/Users/Evan/Dropbox/Code/chickens")
 
 # import all the data
-data <- read.csv("Chicken_Samples_Coordinates_OL_EIP.csv", header=T)
+data <- read.csv("Chicken_Samples_Coordinates_OL_EIP.csv", header=T, strip.white=TRUE, stringsAsFactors=FALSE)
 
 # extract the necessary info, and drop samples with any missing data
-pts <- data.matrix(na.omit(data[c('Longtitude', 'Latitude', 'BP')]))
+pts <- na.omit(data[c('Longtitude', 'Latitude', 'BP')])
 
 # calculate tessellation and triangulation
-vtess <- deldir(pts[,'Longtitude'], pts[,'Latitude'])
+vtess <- deldir(pts$Longtitude, pts$Latitude)
 
 # convert to tile list
 tiles <- tile.list(vtess)
@@ -21,24 +23,13 @@ tiles <- tile.list(vtess)
 par(mar=c(0,0,0,0))
 
 # setup a plotting window, without any points
-plot(pts, asp=1, type="n", bty="n", xlab="", ylab="", axes=FALSE)
+plot(pts$Longtitude, pts$Latitude, asp=1, type="n", bty="n", xlab="", ylab="", axes=FALSE)
 
 # plot the Voronoi diagram from the tile list
-# plot(tiles, fillcol=pts[,'BP'], showpoints=FALSE, add=TRUE, close=FALSE)
+plot(tiles, fillcol=pts[,'BP'], showpoints=FALSE, add=TRUE, close=FALSE)
 
 # add the points
-points(pts, pch=20, cex=0.5, col="black")
+points(pts$Longtitude, pts$Latitude, pch=20, cex=0.5, col="black")
 
-newmap <- getMap(resolution = "low")
-plot(newmap, asp = 1, add=TRUE)
-
-
-# -------------
-
-pts <- pts[1:3,]
-
-par(mar=c(0,0,0,0))
-plot(newmap)
-points(pts[,'Longtitude'], pts[,'Latitude'], cex=0.5, col="red")
-# points(pts[,'Latitude'], pts[,'Longtitude'], cex=0.5, col="green")
-
+# plot the map
+map('world', interior=F, col='gray', add=T)
