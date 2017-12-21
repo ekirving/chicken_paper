b2d95696f98dff96ffe27068fa793d5d8a80350c
Evan Irving-Pease <ekirving@hotmail.com>
2017-12-21 16:51:50 +1100

    almost working

--- temp-veroni2.R
+++ temp-veroni2.R
@@ -14,38 +14,49 @@ library(maptools)
 # -----
 # import all the data
 d <- read.csv("Chicken_Samples_Coordinates_OL_EIP.csv", header=T, strip.white=TRUE, stringsAsFactors=FALSE)
-d <- na.omit(d[1:7,c('BP', 'Longtitude', 'Latitude')])
-colnames(d) <- c('stores', 'lon', 'lat')
+d <- na.omit(d[,c('BP', 'Longtitude', 'Latitude')])
+colnames(d) <- c('BP', 'lon', 'lat')
 # -----
 
 coordinates(d) <- c("lon", "lat")
-proj4string(d) <- CRS("+proj=longlat +datum=WGS84")
+proj4string(d) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0 ")
 
 data(wrld_simpl)
 # regions <- wrld_simpl[wrld_simpl$ISO3 %in% c("regions", "DEU", "AUS"), ]
 # regions <- wrld_simpl[wrld_simpl$REGION %in% c(0, 2, 9, 19, 142, 150), ]
-regions <- wrld_simpl[wrld_simpl$REGION %in% c(2, 9, 19, 142, 150), ]
+regions <- wrld_simpl #[wrld_simpl$REGION %in% c(19, 142, 150), ]
 
 # levels(wrld_simpl$ISO3)
 
 # transform to a planar coordinate reference system (as suggested by @Ege Rubak)
-prj <- CRS("+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80  +units=m")
-d <- spTransform(d, prj)
-regions <- spTransform(regions, prj)
+# prj <- CRS("+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80  +units=m")
+# d <- spTransform(d, prj)
+# regions <- spTransform(regions, prj)
 
 # voronoi function from 'dismo'
 # note the 'ext' argument to spatially extend the diagram
 vor <- dismo::voronoi(d, ext=extent(regions) + 10)
 
-fix <- slot(regions, "polygons")
-regions <- lapply(fix, checkPolygonsHoles)
-
 # use intersect to maintain the attributes of the voronoi diagram
 r <- intersect(vor, regions)
 
-plot(r, col=rainbow(length(r)), lwd=1)
-points(d, pch = 20, col = "black", cex = 1)
+plot(r, col=pts[,'BP'], lwd=1)
+points(d, pch = 20, col = "white", cex = 1)
+points(d, pch = 20, col = "red", cex = 0.5)
 
 # or, to see the names of the areas
-spplot(r, 'stores')
+spplot(r, 'BP')
+
+# ----------
+
+tmp <-map('world', fill=TRUE, plot=FALSE)
+tmp.sp <- map2SpatialPolygons(tmp, IDs=tmp$names)
+
+r <- intersect(vor, tmp.sp)
+
+plot(r, col=pts[,'BP'], lwd=1)
+points(d, pch = 20, col = "white", cex = 1)
+points(d, pch = 20, col = "red", cex = 0.5)
+
+
 
--- veroni-plot.R
+++ veroni-plot.R
@@ -27,12 +27,90 @@ plot(pts$Longtitude, pts$Latitude, asp=1, type="n", bty="n", xlab="", ylab="", a
 # plot the Voronoi diagram from the tile list
 plot(tiles, fillcol=pts[,'BP'], showpoints=FALSE, add=TRUE, close=FALSE)
 
-# add the points
-points(pts$Longtitude, pts$Latitude, pch=20, cex=1, col="white")
-points(pts$Longtitude, pts$Latitude, pch=20, cex=0.5, col="red")
+# raster::mask(interpol.stack, map.polygons)
 
 # plot the map
 map('world', interior=F, col='black', add=T)
 
+# add the points
+points(pts$Longtitude, pts$Latitude, pch=20, cex=1, col="white")
+points(pts$Longtitude, pts$Latitude, pch=20, cex=0.5, col="red")
 
+# ------
+
+SPointsDF_to_voronoi_SPolysDF <- function(sp) {
+
+    # tile.list extracts the polygon data from the deldir computation
+    vor_desc <- tile.list(deldir(sp@coords[,1], sp@coords[,2]))
+
+    lapply(1:(length(vor_desc)), function(i) {
+
+        # tile.list gets us the points for the polygons but we
+        # still have to close them, hence the need for the rbind
+        tmp <- cbind(vor_desc[[i]]$x, vor_desc[[i]]$y)
+        tmp <- rbind(tmp, tmp[1,])
+
+        # now we can make the Polygon(s)
+        Polygons(list(Polygon(tmp)), ID=i)
+
+    }) -> vor_polygons
+
+    # hopefully the caller passed in good metadata!
+    sp_dat <- sp@data
+
+    # this way the IDs _should_ match up w/the data & voronoi polys
+    rownames(sp_dat) <- sapply(slot(SpatialPolygons(vor_polygons),
+                                    'polygons'),
+                               slot, 'ID')
+
+    SpatialPolygonsDataFrame(SpatialPolygons(vor_polygons),
+                             data=sp_dat)
+
+}
+
+require(dplyr)
+
+# remove duplicate points
+pts <- pts %>% group_by(Longtitude, Latitude) %>% summarise(BP = max(BP))
+
+vor_pts <- SpatialPointsDataFrame(pts[c('Longtitude', 'Latitude')], pts, match.ID=TRUE)
+vor <- SPointsDF_to_voronoi_SPolysDF(vor_pts)
+vor_df <- fortify(vor)
+
+tmp <-map('world', fill=TRUE, plot=FALSE)
+tmp.sp <- map2SpatialPolygons(tmp, IDs=tmp$names)
+
+plot(tmp.sp)
+
+# nms <- as.data.frame(tmp$names)
+# rownames(nms) <- tmp$names
+# tmp.spd <- SpatialPolygonsDataFrame(, data=nms)
+# raster::intersect(vor, tmp.spd)
+
+intr <- raster::intersect(vor, tmp.sp)
+
+plot(intr)
+
+# ----------------
+
+# simplify the polgons a tad (tweak 0.00001 to your liking)
+tmp.sp2 <- gSimplify(tmp.sp, tol = 0.00001)
+
+# this is a well known R / GEOS hack (usually combined with the above) to
+# deal with "bad" polygons
+tmp.sp2 <- gBuffer(tmp.sp2, byid=TRUE, width=0)
+
+# merge the adjacent polys
+regionOfInterest <- gUnionCascaded(tmp.sp2)
+
+# plot(tmp.sp)
+plot(regionOfInterest)
+
+intr2 <- raster::intersect(vor, regionOfInterest)
+
+plot(intr2)
+
+# add the points
+points(pts$Longtitude, pts$Latitude, pch=20, cex=1, col="white")
+points(pts$Longtitude, pts$Latitude, pch=20, cex=0.5, col="red")
 
